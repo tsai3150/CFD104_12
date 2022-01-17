9cc21047a4ff94ce5042a7abbaa2c585aab0e25e
tsai3150 <505102470zhen@gmail.com>
2022-01-17 13:59:17 +0800

    修改

---
+++ js/page_js/raiders-base.js
@@ -0,0 +1,24 @@
+function doFirst(){
+    // 先跟HTML產生關聯，再建事件聆聽功能
+    let canvas = document.getElementById('canvas');
+    let context = canvas.getContext('2d');
+
+    context.fillStyle='#82cddb';
+    context.strokeStyle='steelblue';
+    context.lineWidth=10;
+    
+    // 填塞
+    context.fillRect(0,20,650,260);
+    // 挖空
+    context.clearRect(0, 45, 650, 210);
+    context.clearRect(200,20,20,260);
+    context.clearRect(420,20,20,260);
+
+
+    // 繪出
+    context.stroke();
+    
+    // 橡皮擦
+    // context.clearRect(0, 0, canvas.width, canvas.height);
+}
+window.addEventListener('load',doFirst)
